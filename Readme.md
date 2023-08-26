2ec49ae59064e85b25998e81f0505baa69e14df7
Volodymyr Ohorenko <volodymyr.ohorenko@inveritasoft.com>
2023-08-26 13:28:23 +0300

    Mongo init

---
+++ Readme.md
@@ -0,0 +1,7 @@
+ To start project:
+
+``docker compose up --build -d mongodb``
+ 
+Entrance to the container 
+
+ ``docker exec -it <your container name> bash``
---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "root",
+        pwd: "lk123k6k567h8HGas234",
+        roles: [
+            {
+                role: "readWrite",
+                db: "shojo_dev"
+            }
+        ]
+    }
+);
